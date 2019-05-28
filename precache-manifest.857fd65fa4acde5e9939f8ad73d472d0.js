cba69f9d6f43afc8db3265d57e28024c63979cb4
moody blues <toorm@mail.gvsu.edu>
2019-05-28 10:11:21 -0400

    Updates

---
+++ precache-manifest.857fd65fa4acde5e9939f8ad73d472d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df8a5525ee15a183f3957bab812cb71",
+    "url": "/vintool/index.html"
+  },
+  {
+    "revision": "4541e2d5e3c0020acb96",
+    "url": "/vintool/static/css/main.4d0dcd33.chunk.css"
+  },
+  {
+    "revision": "9f57da513c53f5f99015",
+    "url": "/vintool/static/js/2.17e24b86.chunk.js"
+  },
+  {
+    "revision": "4541e2d5e3c0020acb96",
+    "url": "/vintool/static/js/main.78325184.chunk.js"
+  },
+  {
+    "revision": "eb84539af7a8b234fa5b",
+    "url": "/vintool/static/js/runtime~main.037a7723.js"
+  },
+  {
+    "revision": "bed17ad213e1249a71b3ef712b66d87e",
+    "url": "/vintool/static/media/bg.bed17ad2.png"
+  }
+]);
